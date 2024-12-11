f327bde7cbdd3dd49a86c1e44a0fc4fa7ebf48b1
paubt <pau_b@protonmail.com>
2024-12-11 22:48:16 +0100

    init day04

---
+++ 2024/day04/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+
+fn part1(input: String) -> String
+{
+    "todo".to_string()
+}
+
+
+fn part2(input: String) -> String
+{
+    "todo".to_string()
+}
+
+fn read_in_input(path: &str) -> String 
+{
+    fs::read_to_string(path).unwrap()
+}
+
+fn main() 
+{
+    let input = read_in_input("./data/input.txt");
+    println!(
+        "day 3 solution part 1: {} part 2: {}",
+        part1(input.clone()),
+        part2(input)
+    );
+}
